63fe5932f379eeb12e4f41beeae24a2cf55c6230
Prasadvenkat7360 <82456673+Prasadvenkat7360@users.noreply.github.com>
2021-11-04 11:56:35 +0530

    Add files via upload

---
+++ loanSequences.sql
@@ -0,0 +1,33 @@
+--------------------------------------------------------
+--  File created - Wednesday-November-03-2021   
+--------------------------------------------------------
+DROP SEQUENCE "LOAN_APPROVAL"."EMPLOYEE_SEQ";
+DROP SEQUENCE "LOAN_APPROVAL"."FUNCTION_SEQ";
+DROP SEQUENCE "LOAN_APPROVAL"."MENU_SEQ";
+DROP SEQUENCE "LOAN_APPROVAL"."ROLE_FUNCTION_SEQ";
+DROP SEQUENCE "LOAN_APPROVAL"."ROLE_SEQ";
+--------------------------------------------------------
+--  DDL for Sequence EMPLOYEE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "LOAN_APPROVAL"."EMPLOYEE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 323 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence FUNCTION_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "LOAN_APPROVAL"."FUNCTION_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 258 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence MENU_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "LOAN_APPROVAL"."MENU_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence ROLE_FUNCTION_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "LOAN_APPROVAL"."ROLE_FUNCTION_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1990 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence ROLE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "LOAN_APPROVAL"."ROLE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 146 CACHE 20 NOORDER  NOCYCLE ;
